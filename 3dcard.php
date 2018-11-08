63584bfc5e33d065600b4792c7d2c239dbd0ac61
Philip Maaß <PhilipMaasz@aol.com>
2018-11-08 17:52:58 +0100

    initial commit, structure of card

---
+++ 3dcard.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Philipnormal
+ * Date: 08.11.18
+ * Time: 17:14
+ * License: GPL 3.0
+ */
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+
+    <style>
+        .multidimensional-card{
+
+        }
+
+        .multidimensional-card-header{
+
+        }
+
+        .multidimensional-card-body{
+
+        }
+
+        .multidimensional-card-footer{
+
+        }
+    </style>
+</head>
+<body>
+
+<div class="multidimensional-card">
+    <div class="multidimensional-card-header">
+
+    </div>
+
+    <div class="multidimensional-card-body">
+
+    </div>
+
+    <div class="multidimensional-card-footer">
+
+    </div>
+</div>
+
+<script>
+
+</script>
+
+</body>
+</html>
