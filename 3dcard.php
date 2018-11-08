c815b49dade428b8e9d0f1f7539328af83f9e5f9
Philip Maaß <PhilipMaasz@aol.com>
2018-11-08 18:36:59 +0100

    style added

--- 3dcard.php
+++ 3dcard.php
@@ -15,36 +15,112 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
 
     <style>
-        .multidimensional-card{
-
+        :root {
+            --blue: #007bff;
+            --indigo: #6610f2;
+            --purple: #6f42c1;
+            --pink: #e83e8c;
+            --red: #dc3545;
+            --orange: #fd7e14;
+            --yellow: #ffc107;
+            --green: #28a745;
+            --teal: #20c997;
+            --cyan: #17a2b8;
+            --white: #fff;
+            --gray: #6c757d;
+            --gray-dark: #343a40;
+            --primary: #007bff;
+            --secondary: #6c757d;
+            --success: #28a745;
+            --info: #17a2b8;
+            --warning: #ffc107;
+            --danger: #dc3545;
+            --light: #f8f9fa;
+            --dark: #343a40;
+            --breakpoint-xs: 0;
+            --breakpoint-sm: 576px;
+            --breakpoint-md: 768px;
+            --breakpoint-lg: 992px;
+            --breakpoint-xl: 1200px;
+            --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+            --font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
         }
 
-        .multidimensional-card-header{
+        html {
+            font-family: sans-serif;
+            line-height: 1.15;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+            -ms-overflow-style: scrollbar;
+            -webkit-tap-highlight-color: transparent;
+        }
 
+        body {
+            margin: 0;
+            font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.5;
+            color: #212529;
+            text-align: left;
+            background-color: #fff;
+        }
+
+        *, ::after, ::before {
+            box-sizing: border-box;
+        }
+
+        .shadow {
+            box-shadow: 0 .5rem 1rem rgba(0,0,0,.15)!important;
+        }
+
+        .container {
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+
+        .multidimensional-card{
+            position: relative;
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-direction: column;
+            flex-direction: column;
+            min-width: 0;
+            word-wrap: break-word;
+            background-clip: border-box;
+            border: 1px solid rgba(0,0,0,.125);
+            border-radius: .25rem;
+            background-color: #f3f4f1;
+            transform: rotateY(1deg) rotateX(0deg) rotateZ(0deg) scale(1);
         }
 
         .multidimensional-card-body{
-
+            position: relative;
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex: 1 1 auto;
+            flex: 1 1 auto;
+            padding: 1.25rem;
+            margin: 10% 20% 0 0;
+            border: 1px solid rgba(0,0,0,.125);
+            border-radius: .25rem;
+            background-color: white;
+            transform: rotateY(1deg) rotateX(0deg) rotateZ(0deg) scale(1);
         }
 
-        .multidimensional-card-footer{
-
-        }
     </style>
 </head>
 <body>
 
-<div class="multidimensional-card">
-    <div class="multidimensional-card-header">
 
-    </div>
+<div class="container">
+    <div class="multidimensional-card shadow">
+        <div class="multidimensional-card-body shadow">
 
-    <div class="multidimensional-card-body">
-
-    </div>
-
-    <div class="multidimensional-card-footer">
-
+        </div>
     </div>
 </div>
 
